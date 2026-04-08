5bb033d6a3f1f8fbc0c84eb7ef53301f267b3d49
Kristian Zahariev <kristian.zahariev.office@gmail.com>
2026-04-08 15:29:50 +0300

    Add dashboard debug page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import './globals.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'RCX Network',
   description: 'RCX Network',
 }
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="bg">
       <body>{children}</body>
